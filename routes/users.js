f1342f160a76ba0f22741b4a2666a527fe189694
Jae <giffordjacob0@gmail.com>
2022-11-30 12:46:46 +0800

    routing to signup complete. routing to log in complete

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/sign-up', function (req, res, next) {
-  res.send('respond with a resource');
+  res.render('./partials/sign-up');
 });
 
 module.exports = router;
