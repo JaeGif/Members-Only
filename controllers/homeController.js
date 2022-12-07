c46a4d83b6849f90720cd68b69ece42a3d4145e2
Jae <giffordjacob0@gmail.com>
2022-12-07 09:29:40 +0800

    date from when posted is displayed in a more readable way now

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,21 +1,43 @@
 const Post = require('../models/post');
 const User = require('../models/user');
+const relativeTime = require('dayjs/plugin/relativeTime');
+const dayjs = require('dayjs');
+const { Dayjs } = require('dayjs');
+const async = require('async');
 
+dayjs.extend(relativeTime);
 exports.index_get = (req, res, next) => {
   res.redirect('/home');
 };
 
 exports.home_get = (req, res, next) => {
-  Post.find({})
-    .populate('user')
-    .exec(function (err, list_posts) {
-      if (err) {
-        return next(err);
-      }
-      res.render('index', {
-        posts: list_posts,
-      });
-    });
+  async.series(
+    Post.find({})
+      .populate('user')
+      .exec(function (err, list_posts) {
+        if (err) {
+          return next(err);
+        }
+        // Convert DateTime.
+
+        for (let i = 0; i < list_posts.length; i++) {
+          try {
+            let createdFormatted = dayjs().to(dayjs(list_posts[i].createdAt));
+            let updatedFormatted = dayjs().to(dayjs(list_posts[i].updatedAt));
+            list_posts[i] = {
+              ...list_posts[i]._doc,
+              ...{ createdAt: createdFormatted, updatedAt: updatedFormatted },
+            };
+          } catch (error) {
+            console.log(error);
+          }
+        }
+
+        res.render('index', {
+          posts: list_posts,
+        });
+      })
+  );
 };
 
 exports.home_new_post = (req, res, next) => {
