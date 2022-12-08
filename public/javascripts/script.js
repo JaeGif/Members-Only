69f2fd371024eeea40ab6ebdef32da85c9a13daf
Jae <giffordjacob0@gmail.com>
2022-12-08 13:33:05 +0800

    delete client side JS added for smoother experience

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,4 +7,22 @@ function newPost() {
     newBtn.style.display = 'none';
   });
 }
+function deleteBtnModal() {
+  const deleteModalInit = document.getElementsByClassName('delete-popup-init');
+  const deleteModal = document.getElementsByClassName('delete-modal');
+  const closeModal = document.getElementsByClassName('return-delete-btn');
+  const editBtn = document.getElementsByClassName('edit-popup-init');
+
+  for (let i = 0; i < deleteModalInit.length; i++) {
+    deleteModalInit[i].addEventListener('click', () => {
+      deleteModal[i].style.display = 'flex';
+      deleteModalInit[i].style.display = 'none';
+      closeModal[i].addEventListener('click', () => {
+        deleteModal[i].style.display = 'none';
+        deleteModalInit[i].style.display = 'flex';
+      });
+    });
+  }
+}
 newPost();
+deleteBtnModal();
