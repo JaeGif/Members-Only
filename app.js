5efe1ab331187504534c5e10433320d4b5d7ba6e
Jae <giffordjacob0@gmail.com>
2022-12-13 16:06:21 +0800

    build aGAIN

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.$PORT);
-
+/* app.listen(process.env.$PORT);
+ */
 module.exports = app;
 // push 16
